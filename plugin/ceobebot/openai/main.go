ae2e95dd4bd190017f5d8cb5828d38cd8e329bdb
sunist-c <sunist.c@outlook.com>
2024-06-18 11:37:38 +0800

    feat: 为openai添加上下文功能

--- plugin/ceobebot/openai/main.go
+++ plugin/ceobebot/openai/main.go
@@ -9,18 +9,27 @@ import (
 	"github.com/alioth-center/infrastructure/utils/values"
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
+	"time"
 )
 
 var (
-	client openai.Client
-	cfg    OpenPlatformConfig
+	client     openai.Client
+	cfg        OpenPlatformConfig
+	replyCache map[int64]map[int64][]sessionMessage
 )
 
+type sessionMessage struct {
+	role    openai.ChatRoleEnum
+	message string
+	created time.Time
+}
+
 type OpenPlatformConfig struct {
-	Secret openai.Config      `yaml:"secret"`
-	Prompt string             `yaml:"prompt"`
-	Model  string             `yaml:"model"`
-	Price  map[string]float64 `yaml:"price"`
+	Secret    openai.Config      `yaml:"secret"`
+	Prompt    string             `yaml:"prompt"`
+	Model     string             `yaml:"model"`
+	Price     map[string]float64 `yaml:"price"`
+	CacheTime float64            `yaml:"cache_time"`
 }
 
 func init() {
@@ -31,6 +40,7 @@ func init() {
 			panic(err)
 		}
 
+		replyCache = map[int64]map[int64][]sessionMessage{}
 		client = openai.NewClient(cfg.Secret, muteLogger{})
 	})
 }
@@ -47,16 +57,27 @@ func (m Agent) HandleFunc(ctx *zero.Ctx) {
 		return
 	}
 
-	requestMessage := values.BuildStrings("发送人:", ctx.Event.Sender.NickName, "询问信息:", ctx.ExtractPlainText())
+	// 查询缓存信息
+	requestMessage := values.BuildStrings("发送人:", ctx.Event.Sender.NickName, "\n", "发送信息:", ctx.ExtractPlainText())
+	cachedMessages := m.searchCachedMessages(ctx)
+	cachedMessages = append(cachedMessages, sessionMessage{role: openai.ChatRoleEnumUser, message: requestMessage})
+	messages := []openai.ChatMessageObject{{Role: openai.ChatRoleEnumSystem, Content: cfg.Prompt}}
+	for _, cachedMessage := range cachedMessages {
+		messages = append(messages, openai.ChatMessageObject{
+			Role:    cachedMessage.role,
+			Content: cachedMessage.message,
+		})
+	}
+
+	// 请求OpenAI
 	res, err := client.CompleteChat(openai.CompleteChatRequest{
 		Body: openai.CompleteChatRequestBody{
 			Model:     cfg.Model,
-			Messages:  []openai.ChatMessageObject{{Role: "system", Content: cfg.Prompt}, {Role: "user", Content: requestMessage}},
+			Messages:  messages,
 			N:         1,
 			MaxTokens: 1500,
 		},
 	})
-
 	if err != nil {
 		cls.Logger().Error(logger.NewFields(x).WithMessage("ai reply failed").WithData(err.Error()))
 		ctx.SendChain(message.Text("小酒，坏掉了惹...", err.Error()))
@@ -68,6 +89,7 @@ func (m Agent) HandleFunc(ctx *zero.Ctx) {
 		return
 	}
 
+	// 记录日志
 	cost := (cfg.Price["input"]*float64(res.Usage.PromptTokens) + cfg.Price["output"]*float64(res.Usage.CompletionTokens)) / 1000
 	cls.Logger().Info(logger.NewFields(x).
 		WithMessage("ai reply generated").
@@ -79,9 +101,76 @@ func (m Agent) HandleFunc(ctx *zero.Ctx) {
 		WithField("total_token", res.Usage.TotalTokens),
 	)
 
-	ctx.SendChain(message.Text(res.Choices[0].Message.Content))
+	// 将用户信息记录到缓存
+	m.addCacheMessages(ctx, ctx.ExtractPlainText(), res.Choices[0].Message.Content)
+
+	// 返回结果
+	ctx.SendChain(
+		message.Reply(ctx.Event.MessageID),
+		message.Text(res.Choices[0].Message.Content),
+	)
 }
 
 func (m Agent) AttachRules() []zero.Rule {
 	return []zero.Rule{zero.OnlyToMe}
+}
+
+func (m Agent) searchCachedMessages(ctx *zero.Ctx) []sessionMessage {
+	// 查询信息缓存
+	var uid, gid int64
+	if ctx.Event.MessageType == "group" {
+		uid, gid = ctx.Event.UserID, ctx.Event.GroupID
+	} else {
+		uid, gid = ctx.Event.UserID, ctx.Event.UserID
+	}
+
+	// 不存在则创建
+	groupCache, hasGroupCache := replyCache[gid]
+	if !hasGroupCache {
+		replyCache[gid] = map[int64][]sessionMessage{}
+		groupCache = replyCache[gid]
+	}
+	userCache, hasUserCache := groupCache[uid]
+	if !hasUserCache {
+		replyCache[gid][uid] = []sessionMessage{}
+		userCache = replyCache[gid][uid]
+	}
+
+	// 查找未过期的信息，过期则清除
+	var result []sessionMessage
+	for i, sessionMessage := range userCache {
+		if time.Since(sessionMessage.created).Seconds() < cfg.CacheTime && sessionMessage.role == openai.ChatRoleEnumUser {
+			result = append(result, userCache[i], userCache[i+1])
+		}
+	}
+
+	replyCache[gid][uid] = result
+	return result
+}
+
+func (m Agent) addCacheMessages(ctx *zero.Ctx, input, output string) {
+	// 查询信息缓存
+	var uid, gid int64
+	if ctx.Event.MessageType == "group" {
+		uid, gid = ctx.Event.UserID, ctx.Event.GroupID
+	} else {
+		uid, gid = ctx.Event.UserID, ctx.Event.UserID
+	}
+
+	// 不存在则创建
+	groupCache, hasGroupCache := replyCache[gid]
+	if !hasGroupCache {
+		replyCache[gid] = map[int64][]sessionMessage{}
+		groupCache = replyCache[gid]
+	}
+	userCache, hasUserCache := groupCache[uid]
+	if !hasUserCache {
+		replyCache[gid][uid] = []sessionMessage{}
+		userCache = replyCache[gid][uid]
+	}
+
+	// 添加信息
+	userCache = append(userCache, sessionMessage{role: openai.ChatRoleEnumUser, message: input, created: time.Now()})
+	userCache = append(userCache, sessionMessage{role: openai.ChatRoleEnumAssistant, message: output, created: time.Now()})
+	replyCache[gid][uid] = userCache
 }
